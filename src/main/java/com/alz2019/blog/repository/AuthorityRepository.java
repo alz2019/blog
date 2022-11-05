8f2725a97ac26e9c5b93ae46a7385e3b89625a38
alz2019 <az2019git@rambler.ru>
2022-11-05 21:56:26 +0300

    Initial version of blog generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/alz2019/blog/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.alz2019.blog.repository;
+
+import com.alz2019.blog.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/alz2019/blog/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.alz2019.blog.service.dto;
